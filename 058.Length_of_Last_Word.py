23f44d1d20f721f44c45be940c5110e1140e10d6
JonathanWu1120 <jwu166@binghamton.edu>
2017-04-25 23:20:59 -0400

    Length of last word

---
+++ 058.Length_of_Last_Word.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if s == "":
+            return 0
+        a = s.strip().split(" ")
+        return len(a[len(a)-1])
